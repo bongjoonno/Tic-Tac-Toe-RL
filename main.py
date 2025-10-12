806aee700eeba39f48a521f796c55d59536c5e2a
bongjoonno <zachary.feryate@gmail.com>
2025-10-11 19:30:32 -0700

    removed last move from available ones

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from tic_tac_toe_board import Board
 board = Board()
 
 board.display_board()
+
+
+
+# move functions
+
+board.move(0, 0, 'X')
+board.move(0, 0, 'X')
+print(board.available_boxes)
--- tic_tac_toe_board.py
+++ tic_tac_toe_board.py
@@ -3,10 +3,16 @@ class Board():
         self.board = [[0, 0, 0],
                       [0, 0, 0],
                       [0, 0, 0]]
+        
+        self.available_boxes = set((i, j) for i in range(3) for j in range(3))
     
     def display_board(self):
         for row in self.board:
             print(row)
         
     def move(self, y, x, symbol):
-        self.board[y][x] = symbol
+        if (y, x) in self.available_boxes:
+            self.board[y][x] = symbol
+            self.available_boxes.remove((y, x))
+        else:
+            print('Invalid move')
