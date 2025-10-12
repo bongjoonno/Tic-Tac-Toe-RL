a04aa6f637f0f71837fb2e7b5ffeede5082067f7
bongjoonno <zachary.feryate@gmail.com>
2025-10-11 21:26:23 -0700

    testing 1_000_000 random positions

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from random_move import random_move
 board = Board()
 q_table = {}
 
-for _ in range(100_000):
+for _ in range(1_000_000):
     board, q_table = random_move(board, 'X', q_table)
     board, q_table = random_move(board, 'O', q_table)
 
 print(len(q_table))
 
-for pos, val in q_table.items():
-    print(pos, val)
+#for pos, val in q_table.items():
+    #print(pos, val)
