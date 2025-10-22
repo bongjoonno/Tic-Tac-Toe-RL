0ff0a4fcba4f70b3d2c9a429b6b9fe1a2dff5ff6
zachary <zachary.feryate@gmail.com>
2025-10-21 19:21:31 -0700

    fixed update rule in q_learning function

--- q_learning.py
+++ q_learning.py
@@ -1,11 +1,12 @@
 from tic_tac_toe_board import Board
+from constants import GAMMA, ALPHA
 
 def q_learning(board: Board):
     outcome = board.move('X', random = False)
 
     current_q_score = board.q_table[board.last_move_fen]
 
-    board.q_table[board.last_move_fen] = board.last + gamma(max(future_moves) - current_q_score)
+    board.q_table[board.last_move_fen] = ALPHA * (board.last_reward + (GAMMA * (max(future_moves)) - current_q_score))
     
     if 'WON!' in outcome or 'Draw' in outcome:
         board = Board(q_table = board.q_table, epsilon = epsilon)
