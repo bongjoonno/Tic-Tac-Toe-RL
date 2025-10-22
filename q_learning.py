9df6e038ce8c74b5d9646680ef4c31a079a334b7
zachary <zachary.feryate@gmail.com>
2025-10-21 20:03:27 -0700

    fixed board.q_table to Board.q_table in q_learning so python doesn't have to do extra work in looking for the static attribute

--- q_learning.py
+++ q_learning.py
@@ -4,13 +4,13 @@ from constants import ALPHA, GAMMA
 def q_learning(board: Board):
     outcome = board.move('X', random = False)
 
-    current_q_score = board.q_table[board.last_move_fen]
+    current_q_score = Board.q_table[board.last_move_fen]
 
     try:
         max_reward_move_q_score_for_next_state = board.q_learning_update()
     except ValueError:
         max_reward_move_q_score_for_next_state = 0
     
-    board.q_table[board.last_move_fen] += ALPHA * (board.last_reward + (GAMMA * max_reward_move_q_score_for_next_state) - current_q_score)
+    Board.q_table[board.last_move_fen] += ALPHA * (board.last_reward + (GAMMA * max_reward_move_q_score_for_next_state) - current_q_score)
     
     return outcome
