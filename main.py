3365abb38b688a65dc9b2883d44747f8182eaf99
= <=>
2025-10-22 16:49:40 -0700

    starting minimax func

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from imports import choice, sleep
 from tic_tac_toe_board import Board
 from constants import EPSILON
 from q_learning import q_learning
-
+from train_test import train_test
 #q-table function system
 
 epsilon = EPSILON
@@ -11,16 +11,7 @@ board = Board(epsilon)
 
 epochs = 50_000
 
-for i in range(epochs):
-    outcome = q_learning(board)
-    
-    if 'WON!' in outcome or 'Draw' in outcome:
-        board = Board(epsilon)
-        epsilon = max(0, epsilon ** 0.999)
-        continue
-    
-    outcome = board.move('O', move_style = 'choose')
-    
-    if 'WON!' in outcome or 'Draw' in outcome:
-        board = Board(epsilon)
-        epsilon = max(0, epsilon ** 0.999)
+print(len(Board.q_table))
+train_test(epochs, 'train')
+print(len(Board.q_table))
+train_test(epochs, 'play')
---
+++ train_test.py
@@ -0,0 +1,29 @@
+from tic_tac_toe_board import Board
+from q_learning import q_learning
+from constants import EPSILON
+
+def train_test(epochs: int, train_or_test = 'train'):
+    if train_or_test == 'train':
+        move_style = 'random'
+        epsilon = EPSILON
+    elif train_or_test == 'play':
+        move_style = 'choose'
+        epsilon = 0
+    else:
+        raise ValueError("Invalid model-style, choose 'train', or 'play'")
+        
+    board = Board(epsilon)
+
+    for i in range(epochs):
+        outcome = q_learning(board)
+        
+        if 'WON!' in outcome or 'Draw' in outcome:
+            board = Board(epsilon)
+            epsilon = max(0, epsilon ** 0.999)
+            continue
+        
+        outcome = board.move('O', move_style = move_style)
+        
+        if 'WON!' in outcome or 'Draw' in outcome:
+            board = Board(epsilon)
+            epsilon = max(0, epsilon ** 0.999)
