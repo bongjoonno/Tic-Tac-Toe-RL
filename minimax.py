4883e30c3c0fdf0631257d2d9ddcf07f6faafa23
zachary <zachary.feryate@gmail.com>
2025-10-23 19:22:55 -0700

    fixing minimax rn

--- minimax.py
+++ minimax.py
@@ -3,7 +3,7 @@ from imports import deepcopy
 
 def minimax(board: Board, best_action = None):
     if best_action is not None and board.last_reward in [-10, 10]:
-        return (best_action, board.last_reward)
+        return board.last_reward
     
     symbol_to_move = Board.opposite_symbol[board.last_move_player]
     
@@ -14,14 +14,10 @@ def minimax(board: Board, best_action = None):
             board_cop = deepcopy(board)
             board_cop.move(symbol_to_move, {'specific' : a})
             
-            recursed_val = minimax(board_cop, best_action)
+            value = max(value, minimax(board_cop))
+        
+        return value
 
-            if value > recursed_val:
-                best_action = a
-            
-            value = max(value, recursed_val)
-        return board.last_reward
-    
     if symbol_to_move == 'O':
         value = float('inf')
 
@@ -29,13 +25,10 @@ def minimax(board: Board, best_action = None):
             board_cop = deepcopy(board)
             board_cop.move(symbol_to_move, {'specific' : a})
             
-            recursed_val = minimax(board_cop)
-
-            if value < recursed_val:
-                best_action = a
-            
-            value = min(value, recursed_val)
-        return board.last_reward
+            value = min(value, minimax(board_cop))
+        
+        return value
+    
 
 board = Board(0)
 
